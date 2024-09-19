264edb9e57fbe1c1508b578187b03d84f9ebf533
PaoloIncollu <paoloinco@gmail.com>
2024-09-19 09:48:30 +0200

    cambiato titolo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <script>
-/* 
-  Per importare ed utilizzare un componente dentro un altro devo SEMPRE seguire questi 3 passi:
-  1) Importazione del componente
-  2) Dichiarazione del componente
-  3) Utilizzo del componente
-*/
+
 // 1) Importazione del componente
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
